c8cec8e5de487e0997a8d35fb304508d9dc06394
ailin <alexander.ilin.92@gmail.com>
2017-09-12 19:26:42 +0300

    arbitary_nesting_list_to_flat now closer to real world

--- python/arbitary_nesting_list_to_flat.py
+++ python/arbitary_nesting_list_to_flat.py
@@ -1,13 +1,37 @@
-def nested_to_flat(l):
+import collections
+
+
+def __islist(el):
+    return isinstance(el, list)
+
+
+def __isiterable(el):
+    return isinstance(el, collections.Iterable)
+
+
+def __reasonable(el):
+    return isinstance(el, collections.Iterable) and \
+           not isinstance(el, str) and \
+           not isinstance(el, dict)
+
+
+def nested_to_flat(l, predicate=__reasonable):
     for el in l:
-        if isinstance(el, list):
+        if predicate(el):
             yield from nested_to_flat(el)
         else:
             yield el
 
 
 if __name__ == '__main__':
-    l = [[[1,2,3],4,5],[6,],7]
+    l = [
+        [ [1,2,3], 4, 5],
+        [6,],
+        7,
+        '123',
+        {'a': 0, None: False},
+        (i for i in range(5))
+    ]
     flat_l = list(nested_to_flat(l))
-    # [1, 2, 3, 4, 5, 6, 7]
+    # [1, 2, 3, 4, 5, 6, 7, '123', {'a': 0, None: False}, 0, 1, 2, 3, 4]
     print(flat_l)
